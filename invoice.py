dcb00a4fe837033991dfb3b49a6cca2fe75cafb1
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-07-05 17:38:52 +0200

    Don't return a traceback if the password is wrong. Instead return a UserError.
    Task #160407

--- invoice.py
+++ invoice.py
@@ -454,12 +454,15 @@ class Invoice(metaclass=PoolMeta):
 
         def _sign_file(cert, password, request):
             # get key and certificates from PCK12 file
-            (
-                private_key,
-                certificate,
-                additional_certificates,
-                ) = pkcs12.load_key_and_certificates(cert, password)
-
+            try:
+                (
+                    private_key,
+                    certificate,
+                    additional_certificates,
+                    ) = pkcs12.load_key_and_certificates(cert, password)
+            except ValueError:
+                raise UserError(gettext(
+                    'account_invoice_facturae.msg_incorrect_password'))
             # DER is an ASN.1 encoding type
             crt = certificate.public_bytes(serialization.Encoding.DER)
 

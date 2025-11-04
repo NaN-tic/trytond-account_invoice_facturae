c1fa109b073eb77c7d250227a5739a6ea70a9d41
Bernat <bernat@nan-tic.com>
2025-11-04 20:13:25 +0100

    Update xml/xsg file creation and add the possiblity to attach extra files (#16)
    * Add the possibility to attach extra document to an eInvoice.
    
    Task: #181070
    
    * Decide to compress or not the attached extra file depending on size.
    
    Task: 181070
    
    * Add more field in eInvoice report.
    Add the possibility to attach extra documents.
    
    Task: 181070

--- sale.py
+++ sale.py
@@ -10,19 +10,13 @@ class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
 
     # Número d'expedient (Invoice Issue Data Type)
-    file_reference = fields.Char('File Reference', size=20, states={
-            'readonly': Bool(Eval('invoices')),
-        }, depends=['invoices'])
+    file_reference = fields.Char('File Reference', size=20)
     # Número de contracte (Inovice Issue Data Type)
     receiver_contract_reference = fields.Char('Receiver Contract Reference',
-        size=20, states={
-            'readonly': Bool(Eval('invoices')),
-        }, depends=['invoices'])
+        size=20)
     # Operació (Invoice Issue Data Type)
     receiver_transaction_reference = fields.Char(
-        'Receiver Transaction Reference', size=20, states={
-            'readonly': Bool(Eval('invoices')),
-        }, depends=['invoices'])
+        'Receiver Transaction Reference', size=20)
     # Observacions (Invoice Issue Data Type)
     invoice_description = fields.Text('Invoice Description', size=2500,
         states={
@@ -33,27 +27,23 @@ class Sale(metaclass=PoolMeta):
         invoice = super(Sale, self).create_invoice()
 
         if invoice:
+            invoice_description = []
             for line in invoice.lines:
                 if (not line.origin or line.origin.__name__ != 'sale.line'
                         or not line.origin.sale):
                     continue
-                invoice.file_reference = (line.origin.sale.file_reference
-                    or None)
-                invoice.receiver_contract_reference = (
-                    line.origin.sale.receiver_contract_reference or None)
-                if invoice.reference:
-                    invoice.reference = (" - " + (
-                        line.origin.sale.receiver_transaction_reference or ""))
-                else:
-                    invoice.reference = (
-                        line.origin.sale.receiver_transaction_reference
-                        or None)
-                invoice.reference = (
-                    line.origin.sale.receiver_transaction_reference
-                    if (line.origin.sale.receiver_transaction_reference
-                        and not invoice.reference) else None)
-                invoice.invoice_description = (
-                    line.origin.sale.invoice_description or None)
+                if line.origin.sale.invoice_description:
+                    invoice_description.append(
+                        line.origin.sale.invoice_description)
+            invoice.file_reference = (self.file_reference or (
+                    invoice.invoice_address
+                    and invoice.invoice_address.file_reference or None))
+            invoice.receiver_contract_reference = (
+                self.receiver_contract_reference or (invoice.invoice_address
+                    and invoice.invoice_address.receiver_contract_reference
+                    or None))
+            if invoice_description:
+                invoice.invoice_description = "\n".join(invoice_description)
             invoice.save()
         return invoice
 

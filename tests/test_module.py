5df2989f8c68efaa96be382df2b716d63ce6d2e0
Bernat Brunet <bernat@nan-tic.com>
2026-03-31 16:29:46 +0200

    Add discount line structure for the factura-e, based on the extra dependency with the account_invoice_discount module.
    Task: #185671

--- tests/test_module.py
+++ tests/test_module.py
@@ -3,6 +3,7 @@
 
 import os.path
 import secrets
+import xml.etree.ElementTree as ET
 from decimal import Decimal
 from trytond.pool import Pool
 from trytond.transaction import Transaction
@@ -22,10 +23,27 @@ class AccountInvoiceFacturaeTestCase(CompanyTestMixin, ModuleTestCase):
     module = 'account_invoice_facturae'
     depends = ['party_zip']
 
+    @staticmethod
+    def _find_xml_element(root, name):
+        for element in root.iter():
+            if element.tag == name or element.tag.endswith('}%s' % name):
+                return element
+        return None
+
+    @classmethod
+    def _find_xml_text(cls, root, *names):
+        element = root
+        for name in names:
+            element = cls._find_xml_element(element, name)
+            if element is None:
+                return None
+        return element.text
+
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
         activate_module('account_invoice_facturae')
+        activate_module('account_invoice_discount')
         activate_module('sale')
         activate_module('sale_invoice_grouping')
         activate_module('party_zip')
@@ -456,6 +474,218 @@ class AccountInvoiceFacturaeTestCase(CompanyTestMixin, ModuleTestCase):
             invoice.generate_facturae()
             self.assertNotEqual(invoice.invoice_facturae, None)
             self.assertEqual(invoice.invoice_facturae_filename, 'facturae-1.xml')
+
+    @with_transaction()
+    def test_invoice_generation_with_line_discount(self):
+        'Test invoice generation with line discount'
+
+        pool = Pool()
+        Configuration = pool.get('account.configuration')
+        Certificate = pool.get('certificate')
+        Account = pool.get('account.account')
+        FiscalYear = pool.get('account.fiscalyear')
+        Invoice = pool.get('account.invoice')
+        InvoiceLine = pool.get('account.invoice.line')
+        Party = pool.get('party.party')
+        PaymentTerm = pool.get('account.invoice.payment_term')
+        ProductUom = pool.get('product.uom')
+        ProductCategory = pool.get('product.category')
+        ProductTemplate = pool.get('product.template')
+        Product = pool.get('product.product')
+        Tax = pool.get('account.tax')
+        Address = pool.get('party.address')
+        PartyIdentifier = pool.get('party.identifier')
+        Country = pool.get('country.country')
+        Subdivision = pool.get('country.subdivision')
+        PaymentType = pool.get('account.payment.type')
+
+        country = Country(name='Country', code='ES', code3='ESP')
+        country.save()
+        subdivision = Subdivision(
+            name='Subdivision', country=country, code='SUB', type='province')
+        subdivision.save()
+
+        company = create_company()
+        currency = create_currency('EUR')
+        add_currency_rate(currency, 1.0)
+
+        tax_identifier = PartyIdentifier()
+        tax_identifier.type = 'eu_vat'
+        tax_identifier.code = 'BE0897290877'
+        company.header = 'Report Header'
+        company.party.name = 'Seller'
+        company.party.identifiers = [tax_identifier]
+        company.facturae_person_type = 'J'
+        company.facturae_residence_type = 'R'
+        company.party.save()
+        company.save()
+
+        with set_company(company):
+            certificate = Certificate()
+            certificate.name = 'dummy Certificate'
+            with open(os.path.join(
+                        CURRENT_PATH, 'certificate.pfx'), 'rb') as cert_file:
+                certificate.pem_certificate = cert_file.read()
+            certificate.save()
+
+            create_chart(company, tax=True)
+
+            fiscalyear = set_invoice_sequences(get_fiscalyear(company))
+            fiscalyear.save()
+            FiscalYear.create_period([fiscalyear])
+
+            payment_receivable, = PaymentType.create([{
+                    'name': 'Payment Receivable',
+                    'kind': 'receivable',
+                    'company': company.id,
+                    'facturae_type': '01',
+                    }])
+            revenue, = Account.search([
+                    ('type.revenue', '=', True),
+                    ('closed', '=', False),
+                    ], limit=1)
+            tax_account, = Account.search([
+                    ('code', '=', '6.3.6'),
+                    ('closed', '=', False),
+                    ], limit=1)
+            with Transaction().set_user(0):
+                vat21 = Tax()
+                vat21.name = vat21.description = '21% VAT'
+                vat21.type = 'percentage'
+                vat21.rate = Decimal('0.21')
+                vat21.invoice_account = tax_account
+                vat21.report_type = '05'
+                vat21.credit_note_account = tax_account
+
+            vat21.save()
+
+            company_address, = company.party.addresses
+            company_address.street = 'street'
+            company_address.postal_code = '08201'
+            company_address.city = 'City'
+            company_address.subdivision = subdivision
+            company_address.country = country
+            company_address.save()
+            party = Party(name='Buyer')
+            tax_identifier = PartyIdentifier()
+            tax_identifier.type = 'eu_vat'
+            tax_identifier.code = 'BE0897290877'
+            party.identifiers = [tax_identifier]
+            party.save()
+
+            address_dict = {
+                'party': party.id,
+                'street': 'St sample, 15',
+                'city': 'City',
+                'postal_code': '08201',
+                'facturae_person_type': 'J',
+                'facturae_residence_type': 'R',
+                'country': country.id,
+                'subdivision': subdivision.id,
+                }
+            address, = Address.create([address_dict])
+            party.addresses = [address]
+            party.save()
+
+            unit, = ProductUom.search([('name', '=', 'Unit')])
+            product_category = ProductCategory(name='Product Category')
+            product_category.accounting = True
+            product_category.account_revenue = revenue
+            product_category.customer_taxes = [vat21]
+            product_category.save()
+
+            template = ProductTemplate()
+            template.name = 'Product'
+            template.default_uom = unit
+            template.type = 'service'
+            template.list_price = Decimal('20')
+            template.account_category = product_category
+            template.save()
+            product = Product()
+            product.template = template
+            product.save()
+
+            term, = PaymentTerm.create([{
+                        'name': 'Payment term',
+                        'lines': [
+                            ('create', [{
+                                        'type': 'remainder',
+                                        'relativedeltas': [
+                                            ('create', [{
+                                                        'sequence': 0,
+                                                        'days': 0,
+                                                        'months': 0,
+                                                        'weeks': 0,
+                                                        }])],
+                                        }])],
+                        }])
+
+            configuration = Configuration(1)
+            configuration.facturae_service = 'only_file'
+            configuration.save()
+
+            with Transaction().set_user(0):
+                invoice = Invoice()
+                invoice.type = 'out'
+                invoice.party = party
+                invoice.on_change_party()
+                invoice.invoice_address = address
+                invoice.payment_type = payment_receivable
+                invoice.payment_term = term
+                invoice.currency = currency
+                invoice.company = company
+                invoice.set_journal()
+                invoice._update_account()
+
+                line = InvoiceLine()
+                line.company = company
+                line.currency = currency
+                line.account = revenue
+                line.on_change_account()
+                line.product = product
+                line.on_change_product()
+                line.description = 'Test'
+                line.quantity = 1
+                line.base_price = Decimal('10.0000')
+                line.discount_rate = Decimal('0.1')
+                line.on_change_discount_rate()
+
+                invoice.lines = [line]
+                invoice.on_change_lines()
+
+                invoice.save()
+                Invoice.post([invoice])
+
+            invoice.generate_facturae()
+            self.assertIsNotNone(invoice.invoice_facturae)
+
+            root = ET.fromstring(invoice.invoice_facturae)
+            invoice_line = self._find_xml_element(root, 'InvoiceLine')
+            self.assertIsNotNone(invoice_line)
+            self.assertEqual(
+                Decimal(self._find_xml_text(invoice_line, 'UnitPriceWithoutTax')),
+                Decimal('10.0000'))
+            self.assertEqual(
+                Decimal(self._find_xml_text(invoice_line, 'TotalCost')),
+                Decimal('10.0000'))
+            self.assertEqual(
+                self._find_xml_text(
+                    invoice_line, 'DiscountsAndRebates', 'Discount',
+                    'DiscountReason'),
+                'Discount 10%')
+            self.assertEqual(
+                Decimal(self._find_xml_text(
+                    invoice_line, 'DiscountsAndRebates', 'Discount',
+                    'DiscountRate')),
+                Decimal('10'))
+            self.assertEqual(
+                Decimal(self._find_xml_text(
+                    invoice_line, 'DiscountsAndRebates', 'Discount',
+                    'DiscountAmount')),
+                Decimal('1.0000'))
+            self.assertEqual(
+                Decimal(self._find_xml_text(invoice_line, 'GrossAmount')),
+                Decimal('9.0000'))
 
     @with_transaction()
     def test_attachment_mixed_sizes(self):
